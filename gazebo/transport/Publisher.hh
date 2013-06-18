759ef3f70829656f8489df08dcac80de1f662377
William Woodall <william@osrfoundation.org>
2013-06-18 14:19:23 -0700

    Rebase from 'debian/hydro/gazebo'

--- gazebo/transport/Publisher.hh
+++ gazebo/transport/Publisher.hh
@@ -41,7 +41,7 @@ namespace gazebo
     {
       /// Deprecated
       public: Publisher(const std::string &_topic, const std::string &_msgType,
-                        unsigned int _limit, bool _latch) GAZEBO_DEPRECATED;
+                  unsigned int _limit, bool _latch) GAZEBO_DEPRECATED(1.5);
 
       /// \brief Constructor
       /// \param[in] _topic Name of topic to be published
@@ -63,10 +63,21 @@ namespace gazebo
       ///        publisher
       public: void WaitForConnection() const;
 
+      /// \brief Block until a connection has been established with this
+      ///        publisher
+      /// \param[in] _timeout Maxiumum time to wait. Use a negative time
+      /// value to wait forever.
+      /// \return True if a connection was established.
+      public: bool WaitForConnection(const common::Time &_timeout) const;
+
+      /// \brief DEPRECATED in version 1.6
+      /// \sa SetPublication
+      public: void SetPublication(PublicationPtr &_publication, int _i)
+              GAZEBO_DEPRECATED(1.5);
+
       /// \brief Set the publication object for a particular publication
       /// \param[in] _publication Pointer to the publication object to be set
-      /// \param[in] _i Index into publications vector that will be set
-      public: void SetPublication(PublicationPtr &_publication, int _i);
+      public: void SetPublication(PublicationPtr _publication);
 
       /// \brief Publish a protobuf message on the topic
       /// \param[in] _message Message to be published
@@ -102,15 +113,25 @@ namespace gazebo
       /// \brief Send latest message over the wire. For internal use only
       public: void SendMessage();
 
+      /// \brief Set our containing node.
+      /// \param[in] _node Pointer to a node. Should be the node that create
+      /// this publisher.
+      public: void SetNode(NodePtr _node);
+
       /// Deprecated
-      public: bool GetLatching() const GAZEBO_DEPRECATED;
+      public: bool GetLatching() const GAZEBO_DEPRECATED(1.5);
 
       /// \brief Get the previously published message
       /// \return The previously published message, if any
       public: std::string GetPrevMsg() const;
 
+      /// \brief Get the previously published message
+      /// \return The previously published message, if any
+      public: MessagePtr GetPrevMsgPtr() const;
+
       /// \brief Callback when a publish is completed
-      private: void OnPublishComplete();
+      /// \param[in] _id ID associated with the publication.
+      private: void OnPublishComplete(uint32_t _id);
 
       /// \brief Topic on which messages are published.
       private: std::string topic;
@@ -131,20 +152,30 @@ namespace gazebo
       private: bool queueLimitWarned;
 
       /// \brief List of messages to publish.
-      private: std::list<google::protobuf::Message *> messages;
+      private: std::list<MessagePtr> messages;
 
       /// \brief For mutual exclusion.
-      private: mutable boost::recursive_mutex mutex;
+      private: mutable boost::mutex mutex;
 
       /// \brief The publication pointers. One for normal publication, and
       /// one for debug.
-      private: PublicationPtr publications[2];
+      private: PublicationPtr publication;
 
       /// \brief The previous message published. Used for latching topics.
-      private: google::protobuf::Message *prevMsg;
+      private: MessagePtr prevMsg;
+
+      /// \brief Pointer to our containing node.
+      private: NodePtr node;
 
       private: common::Time currentTime;
       private: common::Time prevPublishTime;
+
+      /// \brief True if waiting to here back about a sent message.
+      private: bool waiting;
+
+      /// \brief Current id of the sent message.
+      private: uint32_t pubId;
+      private: std::list<uint32_t> pubIds;
     };
     /// \}
   }
